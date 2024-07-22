003e04b2449d5662fd373243175d482fd120a9d2
popkid-ke <kidreen254@gmail.com>
2024-07-22 06:42:30 +0300

    Update Reboot.js

--- beltah/Reboot.js
+++ beltah/Reboot.js
@@ -19,7 +19,7 @@ const{repondre,ms,dev,superUser}=com;
 
   const {exec}=require("child_process")
 
-    repondre("> BELTAH-MD bot Restarting ⏳");
+    repondre("> SMARTPOPKID-MD bot Restarting ⏳");
 
   exec("pm2 restart all");
   
